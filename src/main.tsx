2696ab8d0bc13bc954a0b3e795db2c55e77d60f5
motsuni <motsuni@yotta-rc.dev>
2025-11-26 17:38:03 +0900

    サービスワーカーを追加し、キャッシュ機能を実装。HTMLメタ情報を更新し、アイコンとマニフェストファイルを追加。

--- src/main.tsx
+++ src/main.tsx
@@ -10,3 +10,16 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     </GameProvider>
   </React.StrictMode>,
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration) => {
+        console.log("SW registered: ", registration);
+      })
+      .catch((error) => {
+        console.log("SW registration failed: ", error);
+      });
+  });
+}
